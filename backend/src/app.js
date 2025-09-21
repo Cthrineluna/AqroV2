e08bef9e0019b9b253f99722bed7903f610f660f
Yvan Maribbay <maribbayyvan0@gmail.com>
2025-09-21 19:16:04 +0800

    Updated the port to 5001

--- backend/src/app.js
+++ backend/src/app.js
@@ -66,7 +66,7 @@ app.get('/', (req, res) => {
 app.use('/api/activities', require('./routes/activityRoutes'));
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
